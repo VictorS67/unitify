6dca61abda7ce27572fe898fc55a29788489d74d
ChenPanXYZ <me@chenpan.xyz>
2022-11-24 19:52:48 -0500

    make auth working and added package.json

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ log = console.log;
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session')
+const bodyParser = require('body-parser')
 
 const connectionString = process.env.MONGODB_URI;
 let mongoDB = connectionString;
@@ -11,10 +13,26 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 const schemas = require('./schema.js');
+const e = require('express');
 
 const User = mongoose.model('users', schemas.userSchema, 'users');
 
 const backend = express();
+
+
+backend.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+
+backend.use(bodyParser.json())
+
+backend.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+
 
 
 /**
@@ -24,10 +42,10 @@ const backend = express();
 backend.get("/user", (req, res) => {
     console.log("Received a request to check if logged in.")
     if (req.session.loggedin) {
-        return res.status(200).json({ "status": 200, "user": req.session.user})
+        return res.status(200).json({ "status": 200, "user": req.session.user })
     }
     else {
-        return res.status(200).json({ "status": 200, "user": undefined})
+        return res.status(200).json({ "status": 200, "user": undefined })
     }
 })
 
@@ -39,44 +57,77 @@ backend.get("/user", (req, res) => {
  * Will set up user object in the session if the user exists and the credentials match.
  * The Request Body includes username & password.
  */
-backend.post('/auth', (req, res) => {
-    User.findOne({username: req.body.username}, function(err, user) {
+backend.post('/auth', async (req, res) => {
+    try {
+
+
+        const username = req.body.username;
+        const password = req.body.password;
+        let user = await User.findOne({ userName: username });
         if (user === null) {
-            return res.status(404).json({'status': 200, 'message': `The user doesn't exist.`});
-        }
-        else if (!Bcrypt.compareSync(req.body.user.password, user.password)) {
-            return res.status(400).json({'status': 400, 'message': `The username and the password don't match.`});
+            return res.status(404).json({ 'status': 404, 'message': `The user doesn't exist.` });
         }
         else {
-            // The credentials match.
-            req.session.loggedin = true
-            req.session.user = {
-                username: user.username,
-                _id: user._id
+            if (password !== user.pass) {
+                return res.status(400).json({ 'status': 400, 'message': `The credentials don't match.` });
             }
-            return res.status(200).json({'status': 200, 'message': `Logged in.`})
+            else {
+                // The credentials match.
+                req.session.loggedin = true
+                req.session.user = {
+                    username: username,
+                    _id: user._id
+                }
+                return res.status(200).json({ 'status': 200, 'message': `Logged in.` })
+            }
+
         }
-    })
-    .catch(error => {
-        return res.status(500).json({'status': 500, 'message': 'The server is down.'})
-    })
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({ 'status': 500, 'message': 'The server is down.' })
+    }
 })
+
+
+/**
+ * DELETE /logout
+ * Logout
+ * Will delete the session
+ */
+ backend.delete('/logout', async (req, res) => {
+    try {
+        if(req.session.user) {
+            req.session.destroy();
+            return res.status(200).json({ 'status': 200, 'message': 'Signed out.' })
+        }
+        else {
+            return res.status(400).json({ 'status': 400, 'message': 'Not logged in.' })
+        }
+    }
+    catch (error) {
+        console.log(error)
+        return res.status(500).json({ 'status': 500, 'message': 'The server is down.' })
+    }
+})
+
+
 
 
 
 
 const port = process.env.PORT || 43030
 backend.listen(port, () => {
-	log(`Listening on port ${port}...`)
+    log(`Listening on port ${port}...`)
 })
 
-   if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  backend.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    backend.use(express.static('client/build'));
 
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  backend.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    backend.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 }
