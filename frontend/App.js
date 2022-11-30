50f385cde9ad86a8154bc7cc97c1c20dfb135112
AlveinTeng <prodigytmx@gmail.com>
2022-11-29 21:17:33 -0500

    try to start with google apis

--- frontend/App.js
+++ frontend/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import { View } from 'react-native';
+import { MaskedViewComponent, View, StyleSheet } from 'react-native';
 import LoginUser from "./Users/LoginUser";
 import MainPage from "./Main/Main";
 import UserPage from "./Users/UserPage";
+import { PROVIDER_GOOGLE, MapView } from "react-native-maps";
 
 
 function App() {
     return (
-        <MainPage/>
+        <View style={styles.container}>
+      <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
+
+        </View>
     );
 }
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
 
+  });
 export default App;
