d9f2f7ba48b29c99c5eb9493d04c5b5284ae1395
VictorS67 <185000048@qq.com>
2022-11-29 21:25:43 -0500

    fix MapView

--- frontend/App.js
+++ frontend/App.js
@@ -3,17 +3,16 @@ import { MaskedViewComponent, View, StyleSheet } from 'react-native';
 import LoginUser from "./Users/LoginUser";
 import MainPage from "./Main/Main";
 import UserPage from "./Users/UserPage";
-import { PROVIDER_GOOGLE, MapView } from "react-native-maps";
-
+import MapView from "react-native-maps";
 
 function App() {
     return (
         <View style={styles.container}>
-      <MapView style={styles.map} provider={PROVIDER_GOOGLE}/>
-
+            <MapView style={styles.map} provider={MapView.PROVIDER_GOOGLE}/>
         </View>
     );
 }
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -23,5 +22,6 @@ const styles = StyleSheet.create({
       height: '100%',
     },
 
-  });
+});
+
 export default App;
