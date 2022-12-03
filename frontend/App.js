cbef729eab4531f41cc797bb77c1d429c2b37c64
VictorS67 <185000048@qq.com>
2022-12-03 02:15:03 -0500

    add waypoints on press

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Text, MaskedViewComponent, View, StyleSheet, Button } from 'react-native';
+import { Text, MaskedViewComponent, View, StyleSheet, Button, TouchableOpacity } from 'react-native';
 import { point } from '@turf/helpers';
 import destination from '@turf/destination';
 import * as Location from "expo-location";
+import MapView, { Marker, Polyline } from "react-native-maps";
+import MapViewDirections from 'react-native-maps-directions';
+import { decode } from "@mapbox/polyline";
+
+import { Ionicons } from '@expo/vector-icons';
 
 import LoginUser from "./Users/LoginUser";
 import MainPage from "./Main/Main";
 import UserPage from "./Users/UserPage";
-import MapView from "react-native-maps";
+import { normalize } from "./Tool/FontSize";
+
+const GOOGLE_MAP_API = "AIzaSyAtc6gbQfdI-YdE7SoIeBXJMPmSV_LuOCk";
 
 function App() {
 
@@ -20,6 +27,7 @@ function App() {
     const [west, sWest] = useState(null); 
     const [north, sNorth] = useState(null);
     const [east, sEast] = useState(null);
+    const [coords, sCoords] = useState([]);
 
     useEffect(() => {
         (async () => {
@@ -56,7 +64,6 @@ function App() {
         sWest(west.geometry.coordinates[0]);
         sNorth(north.geometry.coordinates[1]);
         sEast(east.geometry.coordinates[0]);
-        sPosition(region);
     }
 
     const goToCurrentPosition = () => {
@@ -64,12 +71,57 @@ function App() {
         mapRef.current.animateToRegion(position);
     };
 
-    let text = 'Waiting..';
-    if (errorMsg) {
-        text = errorMsg;
-    } else if (location) {
-        text = JSON.stringify(location);
+    const onMapPress = (e) => {
+
+        const coordinate = e.nativeEvent.coordinate;
+
+        // setCoords(coordinate);
+        mapRef.current.animateToRegion({
+            "latitude": coordinate.latitude, 
+            "longitude": coordinate.longitude, 
+            "latitudeDelta": position.latitudeDelta, 
+            "longitudeDelta": position.longitudeDelta
+        });
+
+        //fetch the coordinates and then store its value into the coords Hook.
+        getDirections(
+            `${position.latitude},${position.longitude}`, 
+            `${coordinate.latitude},${coordinate.longitude}`
+        )
+        .then(
+            coords => {
+                sCoords(coords);
+                console.log(coords);
+            }
+        )
+        .catch(
+            err => {
+                console.log("Something went wrong");
+            }
+        );
     }
+
+    const getDirections = async (startLoc, destinationLoc) => {
+        try {
+            const KEY = GOOGLE_MAP_API; //put your API key here.
+            //otherwise, you'll have an 'unauthorized' error.
+            let resp = await fetch(
+                `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=${KEY}`
+            );
+            let respJson = await resp.json();
+            let points = decode(respJson.routes[0].overview_polyline.points);
+            console.log(points);
+            let coords = points.map((point, index) => {
+                return {
+                    latitude: point[0],
+                    longitude: point[1]
+                };
+            });
+            return coords;
+        } catch (error) {
+            return error;
+        }
+      };
 
     return (
         <View style={styles.container}>
@@ -78,7 +130,8 @@ function App() {
                 position &&
                 <View style={styles.mapContainer}>
                     <MapView 
-                        // onRegionChangeComplete={onRegionChangeComplete}
+                        onRegionChangeComplete={onRegionChangeComplete}
+                        onPress={onMapPress}
                         style={styles.map} 
                         showsUserLocation={true}
                         initialRegion={position}
@@ -92,10 +145,45 @@ function App() {
                         showsTraffic={false}
                         ref={mapRef} //assign our ref to this MapView
                     >
-
+                        {
+                            <Marker
+                                coordinate={{
+                                    "latitude": position.latitude, 
+                                    "longitude": position.longitude
+                                }}
+                                title={"You are here"}
+                                key={"my_location"}
+                            />
+                        }
+                        {
+                            (coords.length >= 2) &&
+                            <Marker
+                                coordinate={coords[coords.length - 1]}
+                                key={`selected_coord`}
+                            />
+                        }
+                        {
+                            (coords.length >= 2) &&
+                            <Polyline 
+                                coordinates={coords}
+                                strokeWidth={6}
+                            />
+                        }
                     </MapView>
 
-                    <Button onPress={() => goToCurrentPosition()} title="Locate Myself" />
+                    <View style={styles.buttonContainer}>
+                        <TouchableOpacity 
+                            style={styles.button} 
+                            onPress={
+                                () => goToCurrentPosition()
+                            }
+                        > 
+                            <Ionicons name="locate" size={normalize(15)} color="black" />
+                            <Text style={styles.buttonText}>
+                                &nbsp;Locate Myself
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
             }
 
@@ -115,12 +203,26 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end"
     },
     map: {
-        ...StyleSheet.absoluteFillObject,
+        ...StyleSheet.absoluteFillObject
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginVertical: normalize(10),
+        backgroundColor: "transparent",
+        alignItems: "center"
     },
     button: {
-
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "dodgerblue",
+        paddingVertical: normalize(5),
+        paddingHorizontal: normalize(10),
+        borderRadius: normalize(20)
+    },
+    buttonText: {
+        fontSize: normalize(13)
     }
-
 });
 
 export default App;
